faeda4ad26dfe7b085c4914b3417f26506da41c1
Ness <6543358+nessben@users.noreply.github.com>
2023-03-03 23:45:49 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-/** c est la fete
+/** c est la fetesdqsdqsd
  * Password hash middleware.
  */
 userSchema.pre('save', async function save(next) {
